21cd7126d09121e4b46b45fe1ba864dd1a7e820e
Anthonijn <0897523@hr.nl>
2015-06-04 12:30:28 +0200

    first app build
    clickable homepage

---
+++ Project1/app/src/main/java/com/example/anthony/project1/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.anthony.project1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaActionSound;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    String TAG = MainActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"Hello");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void newPage(View V){
+
+        Intent intent = new Intent(this, NewGameActivity.class);
+        startActivity(intent);
+    Log.d("startactivity","end of new page method");
+
+        //new page method
+    }
+}
