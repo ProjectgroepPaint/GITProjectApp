37f5c6cebe45e0bb519352f74ad85f154cab2453
Kimberley de Jong <k95dejong@gmail.com>
2015-06-04 15:41:18 +0200

    board class

--- Project1/app/src/main/java/com/example/anthony/project1/Board.java
+++ Project1/app/src/main/java/com/example/anthony/project1/Board.java
@@ -1,27 +1,23 @@
 package com.example.anthony.project1;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class NewGameActivity extends Activity {
+public class Board extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_game);
+        setContentView(R.layout.activity_board);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_game, menu);
+        getMenuInflater().inflate(R.menu.menu_board, menu);
         return true;
     }
 
@@ -38,13 +34,5 @@ public class NewGameActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-
-    }
-
-    public void newPage(View V) {
-
-        Intent intent = new Intent(this, Board.class);
-        startActivity(intent);
-        Log.d("startactivity", "end of new page method");
     }
 }
